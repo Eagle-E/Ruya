4ea832228f6b8542b895c6e2178407d089b88576
Suleyman Guney <guney.enes@hotmail.com>
2022-05-25 15:44:50 +0200

    Added imgui files

--- Ruya/source/TestApplication.hpp
+++ Ruya/source/TestApplication.hpp
@@ -11,6 +11,9 @@
 #include "stb_image.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <memory>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 
 #include "Application.h"
 #include "Window.h"
